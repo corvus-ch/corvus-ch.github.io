d288bdd6accad34ae01f152ccc18eed6ee06bc72
corvus-ch <corvus-ch@users.noreply.github.com>
2021-09-19 17:32:36 +0000

    deploy: d91a622e4b1d1852157cddf2bd0856b273cc11e5

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -2,5 +2,5 @@ var store = [{
         "title": "Bringing a web application to the cloud",
         "excerpt":"Being a web developer for quite some time now, I recently had the change to get involved in a project where we are building a platform as a service (PaaS).Needless to say that I took the changes and tried to deploy a few apps of my own.I found that bringing...","categories": [],
         "tags": [],
-        "url": "/articles/cloud-ready-applications/"
+        "url": "https://corvus-ch.name/articles/cloud-ready-applications/"
       }]
